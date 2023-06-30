9a1019ab4d0d1a63c119801227d3883afa83e842
dexo <dexo1337@pm.me>
2023-06-30 16:32:01 +0300

    - script improve

--- server.js
+++ server.js
@@ -50,18 +50,47 @@ const JsSchema = new Schema({
 
 // Middleware
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
 app.use(compression());
 
-// Remove headers
+// Start server
 app.use((req, res, next) => {
   res.removeHeader("Connection");
   res.removeHeader("Content-Length");
   res.removeHeader("Transfer-Encoding");
   res.removeHeader("X-Powered-By");
   res.setHeader("Cache-Control", "public, max-age=31536000");
+  res.setHeader("Expires", new Date(Date.now() + 31536000000).toUTCString());
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader("Referrer-Policy", "no-referrer-when-downgrade");
+  res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+  if (req.url.includes("/js/")) {
+    res.setHeader("Content-Type", "application/javascript");
+  }
   next();
 });
+
+// Cache middleware
+const cacheMiddleware = (duration) => {
+  return (req, res, next) => {
+    const key = "__express__" + req.originalUrl || req.url;
+    const cachedBody = cache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    }
+    res.sendResponse = res.send;
+    res.send = (body) => {
+      cache.put(key, body, duration * 1000);
+      res.sendResponse(body);
+    };
+    next();
+  };
+};
 
 // Unit converter
 function units(bytes) {
@@ -75,11 +104,13 @@ function units(bytes) {
 }
 
 // Image route - optimize image
-app.get("/images/*", async (req, res) => {
+app.get("/images/:filename", cacheMiddleware(86400), (req, res) => {
   const starttime = new Date().getTime();
   const file = url.parse(req.url).pathname;
   const filePath = path.resolve("./public" + file);
   const cachedFile = cache.get(req.url);
+
+  optimizeImageMiddleware(req, res, filePath, cachedFile, starttime);
 
   if (!fs.existsSync(filePath)) {
     return res.status(404).send({ error: "Not found" });
@@ -89,9 +120,6 @@ app.get("/images/*", async (req, res) => {
     return res.sendFile(cachedFile);
   }
 
-  const optimizedFilePath = await optimizeImage(filePath);
-  cache.put(req.url, optimizedFilePath);
-  res.sendFile(optimizedFilePath);
   if (useMongoDB === "true") {
     const endtime = new Date().getTime();
     const Image = mongoose.model("Image", ImageSchema);
@@ -107,30 +135,93 @@ app.get("/images/*", async (req, res) => {
     });
     image.save();
   }
+
+  const image = sharp(filePath);
+  image.metadata().then((metadata) => {
+    if (metadata.format === "jpeg") {
+      image
+        .jpeg({
+          quality: 85,
+          chromaSubsampling: "4:4:4",
+          force: false,
+        })
+        .toBuffer()
+        .then((data) => {
+          cache.put(req.url, data, 86400000);
+          return res.send(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).send({ error: "Internal server error" });
+        });
+    } else if (metadata.format === "png") {
+      image
+        .png({
+          quality: 85,
+          force: false,
+        })
+        .toBuffer()
+        .then((data) => {
+          cache.put(req.url, data, 86400000);
+          return res.send(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).send({ error: "Internal server error" });
+        });
+    } else if (metadata.format === "webp") {
+      image
+        .webp({
+          quality: 85,
+          force: false,
+        })
+        .toBuffer()
+        .then((data) => {
+          cache.put(req.url, data, 86400000);
+          return res.send(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).send({ error: "Internal server error" });
+        });
+    } else {
+      return res.status(500).send({ error: "Internal server error" });
+    }
+  });
+
+  image.on("error", (err) => {
+    console.log(err);
+    return res.status(500).send({ error: "Internal server error" });
+  });
+
+  image.on("info", (info) => {
+    console.log(info);
+  });
+  
+  image.on("warn", (err) => {
+    console.log(err);
+  });
+
+  image.on("complete", (info) => {
+    return res.send(info);
+  });
 });
 
 // Js route - minify js
-app.get("/js/*", async (req, res) => {
-  const starttime = new Date().getTime();
+app.get("/js/:file", cacheMiddleware(86400), async (req, res) => {
   res.setHeader("Content-Type", "application/javascript");
-  res.setHeader("Cache-Control", "public, max-age=31536000");
 
+  const starttime = new Date().getTime();
   const file = url.parse(req.url).pathname;
   const filePath = path.resolve("./public" + file);
+  const readFile = fs.readFileSync(filePath, "utf8");
 
   if (!fs.existsSync(filePath)) {
     return res.status(404).send("Cannot GET " + file);
   }
 
-  const fileRead = fs.readFileSync(filePath, "utf8");
-  const result = UglifyJS.minify(fileRead);
+  const minifiedBody = UglifyJS.minify(readFile);
 
-  if (result.error) {
-    console.log(result && filePath);
-    return res.status(500).send({ error: "Internal server error" });
-  }
-
-  res.send(result.code);
   if (useMongoDB === "true") {
     const endtime = new Date().getTime();
     const Js = mongoose.model("Js", JsSchema);
@@ -146,6 +237,8 @@ app.get("/js/*", async (req, res) => {
     });
     js.save();
   }
+
+  return res.send(minifiedBody.code);
 });
 
 // Status - for monitoring
@@ -161,20 +254,19 @@ app.get("/status", async (req, res) => {
   }
 });
 
-// Optimize image - if not already optimized
-async function optimizeImage(filePath) {
-  const fileName = path.basename(filePath);
-  const fileDir = path.dirname(filePath);
-  const optimizedFilePath = path.resolve(fileDir, fileName);
+// Optimize image middleware
+const optimizeImageMiddleware = () => {
+  return (req, res, next) => {
+    const imagePath = path.join(__dirname, req.url);
+    sharp(imagePath).toBuffer((err, buffer) => {
+      if (err) return next();
+      res.setHeader("Content-Type", "image/jpeg");
+      res.send(buffer);
+    });
+  };
+};
 
-  if (!fs.existsSync(optimizedFilePath)) {
-    await sharp(filePath).toFile(optimizedFilePath);
-  }
-
-  return optimizedFilePath;
-}
-
-// Start server
+// Start server - if u try to start this and not work, u need to use a proxy like nginx
 app.listen(port, () => {
   console.log(`Loading config from .env file`);
   setTimeout(() => {
